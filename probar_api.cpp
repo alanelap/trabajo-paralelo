13060ee4153be4e311bcde65eb8361b01cb2b883
Alan Opazo <alan.o.garay@gmail.com>
2026-05-22 21:39:08 -0400

    Test: Validacion secuencial de conectividad y endpoints de la API REST

--- probar_api.cpp
+++ probar_api.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <curl/curl.h>
-
+// Prueba unitaria de conectividad HTTP
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
